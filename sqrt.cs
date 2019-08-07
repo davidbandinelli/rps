f09e4084c15b1400dad8abd48bed715260424aad
davidbandinelli <45919059+davidbandinelli@users.noreply.github.com>
2019-08-07 12:13:09 +0200

    FizzBuzz

--- sqrt.cs
+++ sqrt.cs
@@ -7,7 +7,9 @@ using System.Threading.Tasks;
 namespace sqrt {
     class sqrt {
         static void Main(string[] args) {
-            double input = 10;
+            FizzBuzz();
+
+            double input = 17;
             double output = 0;
             double EPSILON = 0.001;
             for (double k = 0.001; k <= input; k += 0.001) {
@@ -19,5 +21,26 @@ namespace sqrt {
             System.Console.WriteLine("Sqrt={0}", output);
             System.Console.ReadKey();
         }
+
+        static void FizzBuzz() {
+            for (int k = 1; k <= 100; k++) {
+                if (k % 3 == 0 && k % 5 == 0) 
+                    System.Console.WriteLine("{0} FizzBuzz", k);
+                else if (k % 3 == 0) 
+                    System.Console.WriteLine("{0} Fizz", k);
+                else if (k % 5 == 0) 
+                    System.Console.WriteLine("{0} Buzz", k);
+                else // non divisibile per 3 o 5
+                    System.Console.WriteLine("{0}", k);
+            }
+            System.Console.ReadKey();
+        }
     }
+
+    /*
+     "Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”."
+
+    */
+
+
 }
